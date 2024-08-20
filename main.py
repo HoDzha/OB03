1eaa4e5b7eb780bd6c2c7933c61af34de3f83de8
HoDzha <hodzha@gmail.com>
2024-08-20 21:47:54 +0500

    adding Bird

--- main.py
+++ main.py
@@ -9,7 +9,26 @@ class Animal():
         print("The animal eats")
     def sleep(self):
         print("The animal sleeps")
-
-
+    def move(self):
+        print("The animal moves")
+    def get_name(self):
+        return self.name
+    def get_age(self):
+        return self.age
+    def get_color(self):
+        return self.color
     def __str__(self):
         return self.name
+
+class Bird(Animal):
+    def __init__(self, name, age, color, wingspan):
+        super().__init__(name, age, color)
+        self.wingspan = wingspan
+    def make_sound(self):
+        print("The bird chirps")
+    def move(self):
+        print("The bird flies")
+    def get_wingspan(self):
+        return self.wingspan
+    def __str__(self):
+        return super().__str__() + " " + str(self.wingspan
